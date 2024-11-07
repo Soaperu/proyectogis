deca073b9f0f9243ef0fbadf56389d40db7d7f49
Jorge Luis YH <jorgeluisyh@gmail.com>
2024-11-06 22:23:19 -0500

    Implementado el diseño de Infromes técnicos toolbar
    El definir esta toolbar permitirá configurar las siguientes toolbars, para ello no olvidar tomar en cuenta los tempalte.xml y el id de template

---
+++ ServicioImplementacionGis/SigcatminProAddin/View/Toolbars/CustomGalleryItem.cs
@@ -0,0 +1,41 @@
+﻿using ArcGIS.Desktop.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace SigcatminProAddin.View.Toolbars
+{
+
+    internal class CustomGalleryItem
+    {
+        internal CustomGalleryItem(string id, string group, IPlugInWrapper plugin)
+        {
+            CommandID = id;
+            PlugInWrapper = plugin;
+            if (PlugInWrapper.LargeImage is ImageSource)
+                Icon32 = (ImageSource)PlugInWrapper.LargeImage;
+            else
+                Icon32 = PlugInWrapper.LargeImage;
+            Group = group;
+        }
+
+        public IPlugInWrapper PlugInWrapper { get; private set; }
+        public object Icon32 { get; private set; }
+
+        public string CommandID { get; private set; }
+
+        public string Group { get; private set; }
+
+        internal void Execute()
+        {
+            if (PlugInWrapper.IsRelevant)
+            {
+                ((ICommand)PlugInWrapper).Execute(null);
+            }
+        }
+    }
+}
