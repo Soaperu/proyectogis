d86f6478a66f350d8e4314805a0358b3ab49663c
Jorge Luis YH <32646673+jorgeluisyh@users.noreply.github.com>
2025-02-11 21:52:17 -0500

    xd

--- ServicioImplementacionGis/SigcatminProAddin/Models/Simultaneidad.cs
+++ ServicioImplementacionGis/SigcatminProAddin/Models/Simultaneidad.cs
@@ -25,7 +25,9 @@ namespace SigcatminProAddin.Models
         private int num_group = 1;
         private string id;
         private string tipo = "s";
-        
+        private List<string> letters = new List<string>();
+
+
         private DatabaseHandler databaseHandler = new DatabaseHandler();
 
         public Simultaneidad(string fecha)
@@ -58,6 +60,7 @@ namespace SigcatminProAddin.Models
             }
         }
 
+
         public void GetQuadrants()
         {
             var zn = codigous.Where(x => x[1] == zone.ToString()).ToList();
@@ -70,6 +73,65 @@ namespace SigcatminProAddin.Models
                 quads.Add(new string[] { row["CD_CUAD"].ToString(), row["CODIGOU"].ToString() });
             }
                         
+        }
+
+        public void PrepareData()
+        {
+            // Obtiene la relación de cuadriculas únicas
+            var keysAll = quads.Select(x => x[0]).Distinct().ToList(); // Ejemplo: ["15-F_1137", "15-F_1138", "15-F_1139"]
+
+            // Obtiene la relación de cuadriculas con todos los derechos traslapados entre sí
+            var rlsTmp = keysAll.ToDictionary(
+                key => key,
+                key => quads.Where(n => n[0] == key).Select(i => i[1]).ToList()
+            );
+
+            // Obtiene aquellas cuadriculas que se intersectan con más de un derecho
+            rls = rlsTmp
+                 .Where(kv => kv.Value.Count > 1)
+                 .ToDictionary(
+                     kv => kv.Key,
+                     kv => kv.Value.OrderBy(v => v).ToList() // Ahora se almacena como List<string>
+                 );
+
+            // Obtiene las agrupaciones únicas de derechos en 'rls'
+            codes = rls.Values.Distinct().ToList(); // Ejemplo: [("010009218", "010016318"), ("010009318", "010016218")]
+
+            // Generar la lista de letras (A-Z, luego combinaciones AA, AB, etc.)
+            letters = Enumerable.Range(0, 26).Select(i => ((char)('A' + i)).ToString()).ToList();
+
+            var extendedLetters = letters
+                .Take(4)
+                .SelectMany(x => letters, (x, i) => x + i)
+                .ToList();
+
+            letters.AddRange(extendedLetters);
+        }
+
+        public void GetGroups()
+        {
+            List<List<string>> groupsTmp  = codes.ToList();
+
+            // Se realiza una iteración sobre todos los valores únicos de derechos
+            foreach (var n in groupsTmp.SelectMany(x => x).Distinct().ToList())
+            {
+                // Se obtiene una lista de todas las agrupaciones que contienen el derecho actual
+                var components = groupsTmp.Where(x => x.Contains(n)).ToList();
+
+                // Se eliminan las agrupaciones de la lista principal
+                foreach (var group in components)
+                {
+                    groupsTmp.Remove(group);
+                }
+
+                // Se agregan a la lista principal todos los valores únicos de la lista "components"
+                groupsTmp.Add(components.SelectMany(x => x).Distinct().ToList());
+            }
+        }
+
+        public void GetSoubgroups()
+        {
+
         }
 
         public void Process(int zone)
